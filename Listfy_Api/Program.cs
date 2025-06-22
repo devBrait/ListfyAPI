6be9831c76efa4a5c8c704a463e26725503dea01
devBrait <lorimiervb@gmail.com>
2025-06-22 20:31:02 -0300

    feat: setup initial database schema with EF migrations

--- Listfy_Api/Program.cs
+++ Listfy_Api/Program.cs
@@ -1,15 +1,18 @@
+using Listfy_Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<DataContext>(x => 
+    x.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
---
+++ Listfy_Infrastructure/Mappings/UserMap.cs
@@ -0,0 +1,41 @@
+using Listfy_Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Listfy_Infrastructure.Mappings;
+
+public class UserMap : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("User");
+        builder.HasKey(x => x.id);
+        builder.Property(x => x.id)
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.name)
+            .IsRequired()
+            .HasColumnType("varchar(100)");
+        
+        builder.Property(x => x.user_name)
+            .IsRequired()
+            .HasColumnType("varchar(100)");
+
+        builder.Property(x => x.email)
+            .IsRequired()
+            .HasColumnType("varchar(255)");
+
+        builder.Property(x => x.password)
+            .IsRequired()
+            .HasColumnType("varchar(255)");
+
+        builder.Property(x => x.created_at)
+            .HasColumnName("CreatedAt")
+            .HasColumnType("timestamp with time zone");
+
+        builder.Property(x => x.updated_at)
+            .HasColumnName("UpdatedAt")
+            .IsRequired(false)
+            .HasColumnType("timestamp with time zone");
+    }
+}
